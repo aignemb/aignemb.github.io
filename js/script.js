5ca960514252e693dbf2eaab4a0e51a0bfb7b6d0
aignemb <aignemb@gmail.com>
2024-11-09 15:23:42 -0500

    Updated nav bar
    -scroll fade
    -added links to nav
    -removed about link

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-let exp = document.getElementById("expand");
-let coll = document.getElementById("collapse");
-let content = document.getElementById("moreaboutme");
+document.addEventListener('DOMContentLoaded', () => {
+    let exp = document.getElementById("expand");
+    let coll = document.getElementById("collapse");
+    let content = document.getElementById("moreaboutme");
 
-exp.addEventListener("click", function () {
-    coll.style.display = "block";
-    exp.style.display = "none";
-    content.style.display = "block";
-});
+    exp.addEventListener("click", function() {
+        coll.style.display = "block";
+        exp.style.display = "none";
+        content.style.display = "block";
+    });
 
-coll.addEventListener("click", function () {
-    coll.style.display = "none";
-    exp.style.display = "block";
-    content.style.display = "none";
+    coll.addEventListener("click", function() {
+        coll.style.display = "none";
+        exp.style.display = "block";
+        content.style.display = "none";
+    });
 });
